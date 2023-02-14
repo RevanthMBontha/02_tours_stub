7464f6caf3e94bde6cde5243af9d567505f006ed
Revanth M. Bontha <69347893+RevanthMBontha@users.noreply.github.com>
2023-02-14 16:21:32 +0530

    added all functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import Loading from './Loading';
 import Tours from './Tours';
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project';
+
+const url =
+  'https://api-for-basic-projects.netlify.app/tours-project/tours-data.json';
+
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const toursData = await response.json();
+      setIsLoading(false);
+      setTours(toursData);
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+        {/* <button onClick={handleToggle}>Toggle Loading</button> */}
+      </main>
+    );
+  }
+  if (!tours.length) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <>
+      <Tours tours={tours} removeTour={removeTour} />
+      {/* <button onClick={handleToggle}>Toggle Loading</button> */}
+    </>
+  );
 }
 
 export default App;
